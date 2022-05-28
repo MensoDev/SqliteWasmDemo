08d42ec2f3b364df3f9b694326a60ea8c6a9d345
MensoDev (Emerson Trindade) <emerson.trindade@codeblox.com.br>
2022-05-27 21:35:25 -0300

    feat: criar projeto de dominio Todo.Domain

---
+++ src/TodoList.Domain/Entities/Todo.cs
@@ -0,0 +1,22 @@
+﻿namespace TodoList.Domain.Entities;
+
+public class Todo
+{
+    public Todo(string title, string description)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+        Completed = false;
+    }
+
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public bool Completed { get; private set; }
+    
+    public void MarkAsCompleted()
+    {
+        Completed = true;
+    }
+}
