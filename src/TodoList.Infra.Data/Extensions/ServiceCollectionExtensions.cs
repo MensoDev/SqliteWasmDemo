b020ea8ace28c229c7225cbdc05e4de9ed522a0a
MensoDev (Emerson Trindade) <emerson.trindade@codeblox.com.br>
2022-05-30 02:55:50 -0300

    feat: progride presentation project e post file

---
+++ src/TodoList.Infra.Data/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TodoList.Infra.Data.Services;
+
+namespace TodoList.Infra.Data.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBlazorWasmDatabaseContextFactory<TContext>( 
+        this IServiceCollection serviceCollection, 
+        Action<DbContextOptionsBuilder>? optionsAction = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton) where TContext : DbContext
+        => AddBlazorWasmDatabaseContextFactory<TContext>(
+            serviceCollection,
+            optionsAction == null ? null : (_, oa) => optionsAction(oa),
+            lifetime);
+
+    public static IServiceCollection AddBlazorWasmDatabaseContextFactory<TContext>(
+        this IServiceCollection serviceCollection,
+        Action<IServiceProvider, DbContextOptionsBuilder>? optionsAction,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        where TContext : DbContext
+    {
+        serviceCollection.TryAdd(
+            new ServiceDescriptor(
+                typeof(IDatabaseStorageService),
+                typeof(BrowserCacheDatabaseStorageService),
+                ServiceLifetime.Singleton));
+
+        serviceCollection.TryAdd(
+            new ServiceDescriptor(
+                typeof(IDatabaseSwapService),
+                typeof(DatabaseSwapService),
+                ServiceLifetime.Singleton));
+
+        serviceCollection.TryAdd(
+            new ServiceDescriptor(
+                typeof(IBlazorWasmDbContextFactory<TContext>),
+                typeof(BlazorWasmDbContextFactory<TContext>),
+                ServiceLifetime.Singleton));
+
+        serviceCollection.AddDbContextFactory<TContext>(
+            optionsAction ?? ((_, _) => { }), lifetime);
+
+        return serviceCollection;
+    }
+}
